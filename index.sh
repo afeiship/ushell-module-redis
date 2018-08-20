b778818a6a1673e0111ae344d73cc7fb029fdc2a
afeiship <1290657123@qq.com>
2018-08-20 20:55:14 +0800

    issue: udpate status

--- index.sh
+++ index.sh
@@ -4,6 +4,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias redis-start='/etc/init.d/redis_6379 start';
 alias redis-stop='/etc/init.d/redis_6379 stop';
-alias redis-check='ps -ef | grep redis';
+alias redis-status='ps -ef | grep redis';
 
 unset ROOT_PATH;
